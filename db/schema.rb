63680eabd91cc645c0f8bbcb94e33b59db1a2a44
Steven Fawcett <steven.fawcett@hpe.com>
2017-05-22 20:13:03 +0100

    Various Changes to get Nav bar working

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,13 @@
 
 ActiveRecord::Schema.define(version: 20170522081107) do
 
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.boolean "active"
